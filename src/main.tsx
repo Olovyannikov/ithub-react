98303971f9d2f3095708ce73dba9155c74e4d35c
Ilya Olovyannikov <ivolovyannikov@sberbank.ru>
2024-11-28 11:09:50 +0300

    add dev configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {BrowserRouter} from "react-router";
-import {ErrorBoundary} from "@/app/providers/ErrorBoundary";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(<StrictMode>
-    <BrowserRouter>
-        <ErrorBoundary>
-            App
-        </ErrorBoundary>
-    </BrowserRouter>
-</StrictMode>)
+root.render(
+    <StrictMode>
+        <BrowserRouter>
+            <ErrorBoundary>App</ErrorBoundary>
+        </BrowserRouter>
+    </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '~': fileURLToPath(new URL('./', import.meta.url)),
+            '@': fileURLToPath(new URL('./src/', import.meta.url)),
+        },
+    },
+});
